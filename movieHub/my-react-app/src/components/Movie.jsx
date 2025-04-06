982a0c4acca953093cbcd5bddbe8cc9aaf2c74b8
shatakshisachan051 <shatakshisachan1017@gmail.com>
2025-04-06 16:41:03 +0530

    6 april

--- movieHub/my-react-app/src/components/Movie.jsx
+++ movieHub/my-react-app/src/components/Movie.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 // url =https://www.omdbapi.com/?i=tt3896198&apikey=d047c95
 
 function Movie(){
 
+    let [movie,setMovie]= useState();
     let [input,setInput]= useState("")
 
     async function handleClick(){
 
-        let res = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=d047c95&y=2006`)
+        let res = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=d047c95&y=2006$s=${input}`)
         console.log(res.data)
 
 
-        displayMovie()
+        setMovie(res.data)
+
+        console.log(movie)
     }
 
-    function displayMovie(){
-
-    }
+   
 
 
 
@@ -26,14 +27,14 @@ function Movie(){
     return (
         <>
         <div>
-            <label htmlFor="name">Enter Movie:</label>
+            <label htmlFor="name" onChange={(inp) => setInput(inp.target.value)} value={input} >Enter Movie:</label>
             <input type="text" placeholder="movie name"/>
             <button onClick={handleClick}>Search</button>
         </div>
 
         <div>
             <h2>Movies</h2>
-        </div>
+                    </div>
         </>
     )
 }
--- tagCleaner/output.js
+++ tagCleaner/output.js
@@ -12,8 +12,25 @@ let res = Input.map((item)=>{
     return item.trim();
 })
 
+let res1 = res.map((item) =>
+  item
+    .split("")
+    .filter(
+      (char) =>
+        (char >= "A" && char <= "Z") ||
+        (char >= "a" && char <= "z") ||
+        (char >= "0" && char <= "9")
+    )
+    .join("").toLowerCase()
+);
+
+let result=res1.join(", ");
+
+console.log(result);
 
 
-console.log(res.join(", "))
+
+
+
 
 
